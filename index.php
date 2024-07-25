09970b91a79b374b71e0c49f4e9bf55f749793b6
giovanirogerio02 <Geovannirogeriogr@gmail.com>
2024-07-25 15:23:29 -0300

    Empresa
    Uma empresa

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    
+//Escreva um algoritmo que receba um valor, calcule e mostre para o
+usuário 5% e 50% deste valor.
+
+
+
+
+
+
+</body>
+
+</html>
